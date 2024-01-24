a819ab40025d2f2773580db10d6559cdc92b08c8
lneoa <leonacheun@gmail.com>
2024-01-24 14:29:24 -0800

    csula24 site upload

---
+++ csula24/script.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    alert("This website is the syllabus for an art/design course at Cal State Los Angeles in Spring 2024. It serves as a web portal for learning resources and class documentation.\n\nThe site was designed by Leona Cheung. The syllabus, lecture slides, assignments, and teaching material is credited to Mindy Seu, Sasha Portis, Sophie Auger, and other instructors.");
+  }
